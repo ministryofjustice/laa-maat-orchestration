1b5f366a71ae49f929206aa948142cb017a08fc2
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2023-11-02 17:47:46 +0000

    LCAM-1023|Create hardship endpoint stubs

--- maat-orchestration/src/test/java/uk/gov/justice/laa/maat/orchestration/MAATOrchestrationApplicationTests.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/maat/orchestration/MAATOrchestrationApplicationTests.java
@@ -1,12 +1,11 @@
 package uk.gov.justice.laa.maat.orchestration;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class MAATOrchestrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
