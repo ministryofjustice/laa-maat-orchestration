7752dcc6ec88ef03d215de9554ee4457269dee4f
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-11-10 17:25:02 +0000

    New service class for calling the Contribution API

---
+++ maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/service/ContributionApiService.java
@@ -0,0 +1,37 @@
+package uk.gov.justice.laa.crime.orchestration.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.crime.commons.client.RestAPIClient;
+import uk.gov.justice.laa.crime.orchestration.config.ServicesConfiguration;
+import uk.gov.justice.laa.crime.orchestration.model.contribution.ApiMaatCalculateContributionRequest;
+import uk.gov.justice.laa.crime.orchestration.model.contribution.ApiMaatCalculateContributionResponse;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ContributionApiService {
+
+    @Qualifier("cccApiClient")
+    private final RestAPIClient contributionApiClient;
+    private final ServicesConfiguration configuration;
+    private static final String RESPONSE_STRING = "Response from Contribution Service: %s";
+
+    public ApiMaatCalculateContributionResponse calculate(ApiMaatCalculateContributionRequest request) {
+        ApiMaatCalculateContributionResponse response = contributionApiClient.post(
+                request,
+                new ParameterizedTypeReference<>() {
+                },
+                configuration.getContributionApi().getContributionEndpoints().getCalculateContributionUrl(),
+                Collections.emptyMap()
+        );
+
+        log.info(String.format(RESPONSE_STRING, response));
+        return response;
+    }
+}
