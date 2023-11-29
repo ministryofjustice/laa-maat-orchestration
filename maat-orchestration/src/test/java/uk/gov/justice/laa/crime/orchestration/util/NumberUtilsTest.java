83133faafffa1a4387570f51306db94422956a33
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-11-29 17:19:23 +0000

    Added missing test scenario in util class

--- maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/util/NumberUtilsTest.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/util/NumberUtilsTest.java
@@ -7,6 +7,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class NumberUtilsTest {
 
     @Test
+    void givenNullLong_whenToIntegerIsInvoked_thenNullIsReturned() {
+        assertThat(NumberUtils.toInteger(null)).isNull();
+    }
+
+    @Test
     void givenLong_whenToIntegerIsInvoked_thenIntValueIsReturned() {
         assertThat(NumberUtils.toInteger(500L)).isEqualTo(500);
     }
