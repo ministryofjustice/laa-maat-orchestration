1a556e5b1f501bf842ce47cc9064378f5ee7b01e
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-01-11 16:24:18 +0000

    added integration tests for find means assessment endpoint

--- maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/integration/MeansAssessmentTest.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/integration/MeansAssessmentTest.java
@@ -1,26 +1,119 @@
 package uk.gov.justice.laa.crime.orchestration.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.justice.laa.crime.orchestration.config.OrchestrationTestConfiguration;
+import uk.gov.justice.laa.crime.orchestration.data.Constants;
+import uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder;
+import uk.gov.justice.laa.crime.orchestration.data.builder.TestModelDataBuilder;
+import uk.gov.justice.laa.crime.orchestration.model.means_assessment.ApiGetMeansAssessmentResponse;
 
+import java.util.Map;
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequest;
+
+@DirtiesContext
+// @TestInstance(TestInstance.Lifecycle.PER_CLASS) TODO: Is this annotation needed?
+@Import(OrchestrationTestConfiguration.class)
+@SpringBootTest(classes = OrchestrationTestConfiguration.class, webEnvironment = DEFINED_PORT)
+@AutoConfigureWireMock(port = 9999)
+// @AutoConfigureObservability TODO: Is this annotation needed? Not sure what it does
 public class MeansAssessmentTest {
+
+    private static final String ENDPOINT_URL = "/api/internal/v1/orchestration/cma";
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private WireMockServer wiremock;
+
+    @BeforeEach
+    public void setUp() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .addFilter(springSecurityFilterChain).build();
+    }
+
+    @AfterEach
+    void cleanUp() {
+        wiremock.resetAll();
+    }
 
     @Test
     void givenValidIds_whenFindIsInvoked_thenAssessmentIsReturned () throws Exception {
-        // TODO: Generate test response object for call to CMA
-        // TODO: Setup wiremock to returned test response object when CMA findUrl is called
-        // TODO: Generate test ids to use in request to endpoint under test
-        // TODO: Use mvc.perform to perform request to find endpoint under test
-        // TODO: Assert that the correct response, type and data is returned in response
+        String response = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiGetMeansAssessmentResponse());
+        String cmaFindAssessmentUrl = String.format(
+                "/api/internal/v1/assessment/means/%s", Constants.FINANCIAL_ASSESSMENT_ID);
+
+        stubForOAuth();
+        wiremock.stubFor(get(urlMatching(cmaFindAssessmentUrl))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(response)));
+
+        String orchFindAssessmentUrl = String.format(
+                "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
+        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(Constants.FINANCIAL_ASSESSMENT_ID))
+                .andExpect(jsonPath("$.initial.id").value(MeansAssessmentDataBuilder.INIT_MEANS_ID));
+    }
+
+    @Test
+    void givenEmptyOAuthToken_whenFindIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
+        String orchFindAssessmentUrl = String.format(
+                "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
+        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl, false))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     void givenInvalidIds_whenFindIsInvoked_thenBadRequestIsReturned() throws Exception {
+        stubForOAuth();
 
+        String orchFindAssessmentUrl = String.format(
+                "%s/%d/applicantId/%s", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, "invalid-id");
+        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void givenUnknownIds_whenFindIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
+    void givenApiClientException_whenFindIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
+        String cmaFindAssessmentUrl = String.format(
+                "/api/internal/v1/assessment/means/%s", Constants.FINANCIAL_ASSESSMENT_ID);
 
+        stubForOAuth();
+        wiremock.stubFor(get(urlMatching(cmaFindAssessmentUrl))
+                .willReturn(WireMock.serverError()));
+
+        String orchFindAssessmentUrl = String.format(
+                "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
+        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+                .andExpect(status().isInternalServerError());
     }
 
     @Test
@@ -29,10 +122,16 @@ public class MeansAssessmentTest {
     }
 
     @Test
+    void givenEmptyOAuthToken_whenCreateIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
+
+    }
+
+    @Test
     void givenInvalidRequestData_whenCreateIsInvoked_thenBadRequestIsReturned() throws Exception {
 
     }
 
+    // TODO: Check that rollback is called?
     @Test
     void givenErrorCallingMaatApi_whenCreateIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
 
@@ -44,12 +143,33 @@ public class MeansAssessmentTest {
     }
 
     @Test
-    void givenInvalidRequestData_whenUpdateIsInvoked_thenBadRequestIsReturned() throws Exception {
+    void givenEmptyOAuthToken_whenUpdateIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
 
     }
 
     @Test
+    void givenInvalidRequestData_whenUpdateIsInvoked_thenBadRequestIsReturned() throws Exception {
+
+    }
+
+    // TODO: Check that rollback is called?
+    @Test
     void givenErrorCallingMaatApi_whenUpdateIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
 
+    }
+
+    // TODO: Can this be located somewhere common for any integration to test to import?
+    private void stubForOAuth() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> token = Map.of(
+                "expires_in", 3600,
+                "token_type", "Bearer",
+                "access_token", UUID.randomUUID()
+        );
+
+        wiremock.stubFor(post("/oauth2/token")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(mapper.writeValueAsString(token))));
     }
 }
