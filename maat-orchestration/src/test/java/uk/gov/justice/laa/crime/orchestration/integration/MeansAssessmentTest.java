27294031bf70bb08874a0e8c5cb622d41e1faab3
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-01-16 13:41:06 +0000

    rest of means assessment flow int tests written, wip bug fixes and tweaking

--- maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/integration/MeansAssessmentTest.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/integration/MeansAssessmentTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,12 +19,15 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import uk.gov.justice.laa.crime.enums.CourtType;
 import uk.gov.justice.laa.crime.orchestration.config.OrchestrationTestConfiguration;
 import uk.gov.justice.laa.crime.orchestration.data.Constants;
 import uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder;
 import uk.gov.justice.laa.crime.orchestration.data.builder.TestModelDataBuilder;
-import uk.gov.justice.laa.crime.orchestration.model.means_assessment.ApiGetMeansAssessmentResponse;
+import uk.gov.justice.laa.crime.orchestration.dto.WorkflowRequest;
+import uk.gov.justice.laa.crime.orchestration.dto.maat.ApplicationDTO;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -32,6 +36,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequest;
+import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequestGivenContent;
 
 @DirtiesContext
 // @TestInstance(TestInstance.Lifecycle.PER_CLASS) TODO: Is this annotation needed?
@@ -42,6 +47,10 @@ import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequest;
 public class MeansAssessmentTest {
 
     private static final String ENDPOINT_URL = "/api/internal/v1/orchestration/cma";
+    private static final String CMA_URL = "/api/internal/v1/assessment/means";
+    private static final String CCP_URL = "/api/internal/v1/proceedings";
+    private static final String CCC_URL = "/api/internal/v1/contribution";
+    private static final String MAAT_API_URL = "/api/internal/v1/assessment/execute-stored-procedure";
     private MockMvc mvc;
     @Autowired
     private ObjectMapper objectMapper;
@@ -66,18 +75,16 @@ public class MeansAssessmentTest {
     @Test
     void givenValidIds_whenFindIsInvoked_thenAssessmentIsReturned () throws Exception {
         String response = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiGetMeansAssessmentResponse());
-        String cmaFindAssessmentUrl = String.format(
-                "/api/internal/v1/assessment/means/%s", Constants.FINANCIAL_ASSESSMENT_ID);
 
         stubForOAuth();
-        wiremock.stubFor(get(urlMatching(cmaFindAssessmentUrl))
+        wiremock.stubFor(get(urlMatching(CMA_URL + "/" + Constants.FINANCIAL_ASSESSMENT_ID))
                 .willReturn(WireMock.ok()
                         .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
                         .withBody(response)));
 
-        String orchFindAssessmentUrl = String.format(
+        String findAssessmentUrl = String.format(
                 "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
-        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+        mvc.perform(buildRequest(HttpMethod.GET, findAssessmentUrl))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(Constants.FINANCIAL_ASSESSMENT_ID))
                 .andExpect(jsonPath("$.initial.id").value(MeansAssessmentDataBuilder.INIT_MEANS_ID));
@@ -85,9 +92,9 @@ public class MeansAssessmentTest {
 
     @Test
     void givenEmptyOAuthToken_whenFindIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
-        String orchFindAssessmentUrl = String.format(
+        String findAssessmentUrl = String.format(
                 "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
-        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl, false))
+        mvc.perform(buildRequest(HttpMethod.GET, findAssessmentUrl, false))
                 .andExpect(status().isUnauthorized());
     }
 
@@ -95,70 +102,237 @@ public class MeansAssessmentTest {
     void givenInvalidIds_whenFindIsInvoked_thenBadRequestIsReturned() throws Exception {
         stubForOAuth();
 
-        String orchFindAssessmentUrl = String.format(
+        String findAssessmentUrl = String.format(
                 "%s/%d/applicantId/%s", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, "invalid-id");
-        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+        mvc.perform(buildRequest(HttpMethod.GET, findAssessmentUrl))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     void givenApiClientException_whenFindIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
-        String cmaFindAssessmentUrl = String.format(
-                "/api/internal/v1/assessment/means/%s", Constants.FINANCIAL_ASSESSMENT_ID);
-
         stubForOAuth();
-        wiremock.stubFor(get(urlMatching(cmaFindAssessmentUrl))
+        wiremock.stubFor(get(urlMatching(CMA_URL + "/" + Constants.FINANCIAL_ASSESSMENT_ID))
                 .willReturn(WireMock.serverError()));
 
-        String orchFindAssessmentUrl = String.format(
+        String findAssessmentUrl = String.format(
                 "%s/%d/applicantId/%d", ENDPOINT_URL, Constants.FINANCIAL_ASSESSMENT_ID, Constants.APPLICANT_ID);
-        mvc.perform(buildRequest(HttpMethod.GET, orchFindAssessmentUrl))
+        mvc.perform(buildRequest(HttpMethod.GET, findAssessmentUrl))
                 .andExpect(status().isInternalServerError());
     }
 
     @Test
     void givenValidRequestData_whenCreateIsInvoked_thenAssessmentIsCreated () throws Exception {
+        String cmaResponse = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiMeansAssessmentResponse());
+        String ccpResponse = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiUpdateApplicationResponse());
+        String cccCalculateResponse = objectMapper.writeValueAsString(TestModelDataBuilder.getApiMaatCalculateContributionResponse());
+        String cccSummariesResponse = objectMapper.writeValueAsString(List.of(TestModelDataBuilder.getApiContributionSummary()));
+        // applicationDTO is overwritten by the applicationDTO below following first call to invokeSP
+        // when this then gets mapped as part of calculate contribution it look through the list of outcomeDTOs and gets last one
+        // mapper assumes last should be an appealOutcome however in test data we are only setting one outcome
+        // this has outcome of CONVICTED which is not a valid appealOutcome (poss as we are only setting one outcome in test data)
+        // Is only one outcome in list valid? If so then mapper should be updated as it assumes there should always be 2 outcomes
+        ApplicationDTO applicationDTO = TestModelDataBuilder.getApplicationDTO(CourtType.CROWN_COURT);
+//        applicationDTO.setMagsOutcomeDTO(TestModelDataBuilder.getOutcomeDTO());
+        String maatApiResponse = objectMapper.writeValueAsString(applicationDTO);
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
 
+        stubForOAuth();
+        wiremock.stubFor(post(urlMatching(CMA_URL))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cmaResponse)));
+        wiremock.stubFor((put(urlMatching(CCP_URL))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(ccpResponse))));
+        wiremock.stubFor(post(urlMatching(CCC_URL + "/calculate-contribution"))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cccCalculateResponse)));
+        wiremock.stubFor(get(urlMatching(CCC_URL + "/summaries"))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cccSummariesResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs(Scenario.STARTED)
+                .willSetStateTo("DB_GET_APPLICATION_CORRESPONDENCE")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_GET_APPLICATION_CORRESPONDENCE")
+                .willSetStateTo("DB_ASSESSMENT_POST_PROCESSING_PART_1_C3")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_ASSESSMENT_POST_PROCESSING_PART_1_C3")
+                .willSetStateTo("DB_PRE_UPDATE_CC_APPLICATION")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_PRE_UPDATE_CC_APPLICATION")
+                .willSetStateTo("DB_ASSESSMENT_POST_PROCESSING_PART_2")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_ASSESSMENT_POST_PROCESSING_PART_2")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, requestBody, ENDPOINT_URL))
+                .andExpect(status().isOk());
+        // TODO: Verify the various stubs were called x amount of times
     }
 
     @Test
     void givenEmptyOAuthToken_whenCreateIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
 
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, requestBody, ENDPOINT_URL, false))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     void givenInvalidRequestData_whenCreateIsInvoked_thenBadRequestIsReturned() throws Exception {
+        WorkflowRequest request = MeansAssessmentDataBuilder.buildWorkFlowRequest();
+        request.setApplicationDTO(null);
+        String requestBody = objectMapper.writeValueAsString(request);
 
+        stubForOAuth();
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, requestBody, ENDPOINT_URL))
+                .andExpect(status().isBadRequest());
     }
 
-    // TODO: Check that rollback is called?
     @Test
     void givenErrorCallingMaatApi_whenCreateIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
 
+        stubForOAuth();
+        wiremock.stubFor(post(urlMatching(CMA_URL))
+                .willReturn(WireMock.serverError()));
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, requestBody, ENDPOINT_URL))
+                .andExpect(status().isInternalServerError());
     }
 
     @Test
     void givenValidRequestData_whenUpdateIsInvoked_thenAssessmentIsUpdated () throws Exception {
+        String cmaResponse = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiMeansAssessmentResponse());
+        String ccpResponse = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.getApiUpdateApplicationResponse());
+        String cccCalculateResponse = objectMapper.writeValueAsString(TestModelDataBuilder.getApiMaatCalculateContributionResponse());
+        String cccSummariesResponse = objectMapper.writeValueAsString(List.of(TestModelDataBuilder.getApiContributionSummary()));
+        // applicationDTO is overwritten by the applicationDTO below following first call to invokeSP
+        // when this then gets mapped as part of calculate contribution it look through the list of outcomeDTOs and gets last one
+        // mapper assumes last should be an appealOutcome however in test data we are only setting one outcome
+        // this has outcome of CONVICTED which is not a valid appealOutcome (poss as we are only setting one outcome in test data)
+        // Is only one outcome in list valid? If so then mapper should be updated as it assumes there should always be 2 outcomes
+        ApplicationDTO applicationDTO = TestModelDataBuilder.getApplicationDTO(CourtType.CROWN_COURT);
+//        applicationDTO.setMagsOutcomeDTO(TestModelDataBuilder.getOutcomeDTO());
+        String maatApiResponse = objectMapper.writeValueAsString(applicationDTO);
 
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
+
+        stubForOAuth();
+        wiremock.stubFor(put(urlMatching(CMA_URL))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cmaResponse)));
+        wiremock.stubFor((put(urlMatching(CCP_URL))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(ccpResponse))));
+        wiremock.stubFor(post(urlMatching(CCC_URL + "/calculate-contribution"))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cccCalculateResponse)));
+        wiremock.stubFor(get(urlMatching(CCC_URL + "/summaries"))
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(cccSummariesResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs(Scenario.STARTED)
+                .willSetStateTo("DB_GET_APPLICATION_CORRESPONDENCE")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_GET_APPLICATION_CORRESPONDENCE")
+                .willSetStateTo("DB_ASSESSMENT_POST_PROCESSING_PART_1_C3")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_ASSESSMENT_POST_PROCESSING_PART_1_C3")
+                .willSetStateTo("DB_PRE_UPDATE_CC_APPLICATION")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_PRE_UPDATE_CC_APPLICATION")
+                .willSetStateTo("DB_ASSESSMENT_POST_PROCESSING_PART_2")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+        wiremock.stubFor(post(urlMatching(MAAT_API_URL))
+                .inScenario("invokeStoredProcedure")
+                .whenScenarioStateIs("DB_ASSESSMENT_POST_PROCESSING_PART_2")
+                .willReturn(WireMock.ok()
+                        .withHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON))
+                        .withBody(maatApiResponse)));
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.PUT, requestBody, ENDPOINT_URL))
+                .andExpect(status().isOk());
+        // TODO: Verify the various stubs were called x amount of times
     }
 
     @Test
     void givenEmptyOAuthToken_whenUpdateIsInvoked_thenUnauthorizedAccessIsReturned() throws Exception {
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
 
+        mvc.perform(buildRequestGivenContent(HttpMethod.PUT, requestBody, ENDPOINT_URL, false))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     void givenInvalidRequestData_whenUpdateIsInvoked_thenBadRequestIsReturned() throws Exception {
+        WorkflowRequest request = MeansAssessmentDataBuilder.buildWorkFlowRequest();
+        request.setApplicationDTO(null);
+        String requestBody = objectMapper.writeValueAsString(request);
 
+        stubForOAuth();
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.PUT, requestBody, ENDPOINT_URL))
+                .andExpect(status().isBadRequest());
     }
 
-    // TODO: Check that rollback is called?
     @Test
     void givenErrorCallingMaatApi_whenUpdateIsInvoked_thenInternalServerErrorIsReturned() throws Exception {
+        String requestBody = objectMapper.writeValueAsString(MeansAssessmentDataBuilder.buildWorkFlowRequest());
 
+        stubForOAuth();
+        wiremock.stubFor(put(urlMatching(CMA_URL))
+                .willReturn(WireMock.serverError()));
+
+        mvc.perform(buildRequestGivenContent(HttpMethod.PUT, requestBody, ENDPOINT_URL))
+                .andExpect(status().isInternalServerError());
     }
 
-    // TODO: Can this be located somewhere common for any integration to test to import?
+    // TODO: Can this be located somewhere common for any integration test to import?
+    // Need to pass in wiremock object if located in common
     private void stubForOAuth() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> token = Map.of(
