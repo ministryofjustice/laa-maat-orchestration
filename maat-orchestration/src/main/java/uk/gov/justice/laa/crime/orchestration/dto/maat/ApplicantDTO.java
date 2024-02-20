1d9db692fcf11269c22ae4c123e1a58e1ab76b92
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-02-20 13:02:15 +0000

    Remove custom deserializer that is no longer required

--- maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/dto/maat/ApplicantDTO.java
+++ maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/dto/maat/ApplicantDTO.java
@@ -1,11 +1,9 @@
 package uk.gov.justice.laa.crime.orchestration.dto.maat;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import uk.gov.justice.laa.crime.orchestration.jackson.SysGenLongDeserializer;
 
 import java.util.Collection;
 import java.util.Date;
@@ -15,7 +13,6 @@ import java.util.Date;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ApplicantDTO extends GenericDTO {
-    @JsonDeserialize(using = SysGenLongDeserializer.class)
     private Long id;
     private String firstName;
     private String otherNames;
