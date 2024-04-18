8b71ee5f7327fbf93e3260e13af9572fde32d6d6
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2024-04-18 16:16:39 +0100

    LCAM-1314:Enabled context propagation to automatically wrap reactor internal mechanisms to propagate context between operators, threads etc. on application startup.

--- maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/MAATOrchestrationApplication.java
+++ maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/MAATOrchestrationApplication.java
@@ -3,12 +3,14 @@ package uk.gov.justice.laa.crime.orchestration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class MAATOrchestrationApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(MAATOrchestrationApplication.class, args);
 	}
 
