a661a5a0b729e9510d24d719a38079b73b8b9dd0
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-09-13 09:26:10 +0100

    Reformatting

--- maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/common/Constants.java
+++ maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/common/Constants.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.laa.crime.orchestration.common;
 
 public final class Constants {
-    public static final String WRN_MSG_REASSESSMENT	= "A reassessment is required";
+    public static final String WRN_MSG_REASSESSMENT = "A reassessment is required";
     public static final String WRN_MSG_INCOMPLETE_ASSESSMENT = "Incomplete assessment has been cleared due to this change.";
 
 }
