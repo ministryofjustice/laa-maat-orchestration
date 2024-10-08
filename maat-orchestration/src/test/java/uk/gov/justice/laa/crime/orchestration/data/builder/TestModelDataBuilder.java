09b6e7583c336468c7aebbe5886f39bed9d57521
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-10-08 20:16:10 +0100

    Test data to cover unlinked applicant logic

--- maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/data/builder/TestModelDataBuilder.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/data/builder/TestModelDataBuilder.java
@@ -4,14 +4,28 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.crime.common.model.common.ApiCrownCourtOutcome;
 import uk.gov.justice.laa.crime.common.model.common.ApiUserSession;
-import uk.gov.justice.laa.crime.common.model.orchestration.common.ApiCrownCourtSummary;
-import uk.gov.justice.laa.crime.common.model.orchestration.common.ApiRepOrderCrownCourtOutcome;
 import uk.gov.justice.laa.crime.common.model.contribution.ApiMaatCalculateContributionResponse;
 import uk.gov.justice.laa.crime.common.model.contribution.common.ApiContributionSummary;
+import uk.gov.justice.laa.crime.common.model.orchestration.common.ApiCrownCourtSummary;
+import uk.gov.justice.laa.crime.common.model.orchestration.common.ApiRepOrderCrownCourtOutcome;
 import uk.gov.justice.laa.crime.common.model.orchestration.court_data_api.hardship.ApiHardshipDetail;
 import uk.gov.justice.laa.crime.common.model.orchestration.court_data_api.hardship.ApiHardshipProgress;
-import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.*;
-import uk.gov.justice.laa.crime.common.model.orchestration.hardship.*;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiFinancialAssessment;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiHardshipOverview;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiIOJAppeal;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiPassportAssessment;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiUpdateApplicationRequest;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiUpdateApplicationResponse;
+import uk.gov.justice.laa.crime.common.model.orchestration.crown_court.ApiUpdateCrownCourtResponse;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.ApiFindHardshipResponse;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.ApiPerformHardshipRequest;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.ApiPerformHardshipResponse;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.DeniedIncome;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.ExtraExpenditure;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.HardshipMetadata;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.HardshipProgress;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.HardshipReview;
+import uk.gov.justice.laa.crime.common.model.orchestration.hardship.SolicitorCosts;
 import uk.gov.justice.laa.crime.enums.*;
 import uk.gov.justice.laa.crime.orchestration.data.Constants;
 import uk.gov.justice.laa.crime.orchestration.dto.WorkflowRequest;
@@ -21,19 +35,34 @@ import uk.gov.justice.laa.crime.orchestration.dto.maat_api.RepOrderCCOutcomeDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.maat_api.RepOrderDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.maat_api.RoleDataItemDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.validation.ReservationsDTO;
-import uk.gov.justice.laa.crime.orchestration.dto.validation.UserSummaryDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.validation.UserActionDTO;
+import uk.gov.justice.laa.crime.orchestration.dto.validation.UserSummaryDTO;
 import uk.gov.justice.laa.crime.orchestration.enums.Action;
-
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.*;
-import java.util.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.*;
+import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.CRITERIA_DETAIL_ID;
+import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.SECTION;
+import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.TOTAL_AGGREGATED_INCOME;
+import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.getReviewTypeDTO;
+import static uk.gov.justice.laa.crime.orchestration.data.builder.MeansAssessmentDataBuilder.getSectionSummaryDTO;
 import static uk.gov.justice.laa.crime.util.DateUtil.toDate;
 import static uk.gov.justice.laa.crime.util.DateUtil.toZonedDateTime;
 
@@ -457,9 +486,9 @@ public class TestModelDataBuilder {
                                 .repId(123L)
                                 .timestamp(timestampToUse)
                                 .statusDTO(RepStatusDTO
-                                    .builder()
-                                    .updateAllowed(true)
-                                    .build())
+                                        .builder()
+                                        .updateAllowed(true)
+                                        .build())
                                 .build()).build();
     }
 
@@ -525,9 +554,16 @@ public class TestModelDataBuilder {
     }
 
     private static Collection<ApplicantLinkDTO> getApplicantLinks() {
+        ApplicantDTO applicantDTO = getApplicantDTO();
+        ApplicantDTO partner = getApplicantDTO();
+        partner.setId(1234L);
         return List.of(ApplicantLinkDTO.builder()
-                .partnerDTO(getApplicantDTO())
-                .build());
+                        .partnerDTO(applicantDTO)
+                        .unlinked(DATE_COMPLETED)
+                        .build(),
+                ApplicantLinkDTO.builder()
+                        .partnerDTO(partner)
+                        .build());
     }
 
     public static ChildWeightingDTO getChildWeightingDTO() {
