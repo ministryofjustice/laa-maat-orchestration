64321177133b8d2e445ec2c7bbf135637bd695f7
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-12-18 16:02:17 +0000

    Add unit tests for EvidenceApiService

--- maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/service/api/EvidenceApiServiceTest.java
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/service/api/EvidenceApiServiceTest.java
@@ -8,12 +8,18 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.laa.crime.common.model.evidence.ApiCreateIncomeEvidenceRequest;
+import uk.gov.justice.laa.crime.common.model.evidence.ApiUpdateIncomeEvidenceRequest;
+import uk.gov.justice.laa.crime.common.model.evidence.ApiUpdateIncomeEvidenceResponse;
 import uk.gov.justice.laa.crime.commons.client.RestAPIClient;
 import uk.gov.justice.laa.crime.orchestration.config.MockServicesConfiguration;
 import uk.gov.justice.laa.crime.orchestration.config.ServicesConfiguration;
 
-import static org.mockito.ArgumentMatchers.*;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EvidenceApiServiceTest {
@@ -30,5 +36,15 @@ class EvidenceApiServiceTest {
     void givenValidRequest_whenCreateEvidenceIsInvoked_thenApiCreateIncomeEvidenceResponseIsReturned() {
         evidenceApiService.createEvidence(new ApiCreateIncomeEvidenceRequest());
         verify(evidenceApiClient).post(any(ApiCreateIncomeEvidenceRequest.class), any(), any(), any());
+    }
+
+    @Test
+    void givenValidRequest_whenUpdateEvidenceIsInvoked_thenApiUpdateIncomeEvidenceResponseIsReturned() {
+        ApiUpdateIncomeEvidenceResponse expectedResponse = new ApiUpdateIncomeEvidenceResponse()
+                .withAllEvidenceReceivedDate(LocalDate.now());
+        when(evidenceApiClient.put(any(ApiUpdateIncomeEvidenceRequest.class), any(), any(), any()))
+                .thenReturn(expectedResponse);
+        var actualResponse = evidenceApiService.updateEvidence(new ApiUpdateIncomeEvidenceRequest());
+        assertThat(actualResponse).isEqualTo(expectedResponse);
     }
 }
