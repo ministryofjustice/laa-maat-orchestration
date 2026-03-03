283db463667a3b6ea141e671dcd3a820519363e0
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2026-03-03 09:49:20 +0000

    test: added unit tests for passport assessment controller

---
+++ maat-orchestration/src/test/java/uk/gov/justice/laa/crime/orchestration/controller/PassportAssessmentControllerTest.java
@@ -0,0 +1,59 @@
+package uk.gov.justice.laa.crime.orchestration.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.justice.laa.crime.orchestration.data.Constants.*;
+import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequestWithTransactionId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.justice.laa.crime.orchestration.config.OrchestrationTestConfiguration;
+import uk.gov.justice.laa.crime.orchestration.data.builder.PassportAssessmentDataBuilder;
+import uk.gov.justice.laa.crime.orchestration.dto.maat.PassportedDTO;
+import uk.gov.justice.laa.crime.orchestration.service.orchestration.PassportAssessmentOrchestrationService;
+import uk.gov.justice.laa.crime.orchestration.tracing.TraceIdHandler;
+
+@WebMvcTest(PassportAssessmentController.class)
+@Import(OrchestrationTestConfiguration.class)
+@AutoConfigureMockMvc(addFilters = false)
+class PassportAssessmentControllerTest {
+
+    private static final String ENDPOINT_URL = "/api/internal/v1/orchestration/passport";
+
+    @Autowired
+    private MockMvc mvc;
+    @MockitoBean
+    private PassportAssessmentOrchestrationService passportAssessmentOrchestrationService;
+    @MockitoBean
+    private TraceIdHandler traceIdHandler;
+
+    @Test
+    void givenValidLegacyId_whenFindIsInvoked_thenOkResponseIsReturned() throws Exception {
+        PassportedDTO dto = PassportAssessmentDataBuilder.getPassportedDTO();
+
+        when(passportAssessmentOrchestrationService.find(PASSPORT_ASSESSMENT_ID)).thenReturn(dto);
+
+        mvc.perform(buildRequestWithTransactionId(HttpMethod.GET,
+                ENDPOINT_URL + "/" + PASSPORT_ASSESSMENT_ID, false))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void givenInvalidLegacyId_whenFindIsInvoked_thenBadRequestResponseIsReturned()
+        throws Exception {
+        mvc.perform(buildRequestWithTransactionId(HttpMethod.GET,
+                ENDPOINT_URL + "/inv4l1d1d", false))
+            .andExpect(status().isBadRequest());
+    }
+
+}
