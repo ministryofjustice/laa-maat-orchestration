a092f5ac84d48199386b5c9664e4c82718c75ca5
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2026-05-01 11:25:35 +0100

    wip changes to post processing of create passport assessment

--- maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/service/orchestration/PassportAssessmentOrchestrationService.java
+++ maat-orchestration/src/main/java/uk/gov/justice/laa/crime/orchestration/service/orchestration/PassportAssessmentOrchestrationService.java
@@ -3,6 +3,10 @@ package uk.gov.justice.laa.crime.orchestration.service.orchestration;
 import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import uk.gov.justice.laa.crime.common.model.tracking.ApplicationTrackingOutputResult;
+import uk.gov.justice.laa.crime.common.model.tracking.ApplicationTrackingOutputResult.AssessmentType;
+import uk.gov.justice.laa.crime.common.model.tracking.ApplicationTrackingOutputResult.RequestSource;
+import uk.gov.justice.laa.crime.enums.NewWorkReason;
 import uk.gov.justice.laa.crime.enums.orchestration.StoredProcedure;
 import uk.gov.justice.laa.crime.orchestration.dto.WorkflowRequest;
 import uk.gov.justice.laa.crime.orchestration.dto.maat.ApplicationDTO;
@@ -11,7 +15,10 @@ import uk.gov.justice.laa.crime.orchestration.dto.maat.PassportedDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.maat_api.RepOrderDTO;
 import uk.gov.justice.laa.crime.orchestration.dto.validation.UserActionDTO;
 import uk.gov.justice.laa.crime.orchestration.exception.MaatOrchestrationException;
+import uk.gov.justice.laa.crime.orchestration.mapper.ApplicationTrackingMapper;
 import uk.gov.justice.laa.crime.orchestration.mapper.PassportAssessmentMapper;
+import uk.gov.justice.laa.crime.orchestration.service.ApplicationService;
+import uk.gov.justice.laa.crime.orchestration.service.ApplicationTrackingDataService;
 import uk.gov.justice.laa.crime.orchestration.service.AssessmentSummaryService;
 import uk.gov.justice.laa.crime.orchestration.service.ContributionService;
 import uk.gov.justice.laa.crime.orchestration.service.MaatCourtDataService;
@@ -35,6 +42,9 @@ public class PassportAssessmentOrchestrationService {
     private final ContributionService contributionService;
     private final MaatCourtDataService maatCourtDataService;
     private final AssessmentSummaryService assessmentSummaryService;
+    private final ApplicationService applicationService;
+    private final ApplicationTrackingMapper applicationTrackingMapper;
+    private final ApplicationTrackingDataService applicationTrackingDataService;
 
 
     public PassportedDTO find(int id) {
@@ -42,10 +52,11 @@ public class PassportAssessmentOrchestrationService {
     }
 
     public ApplicationDTO create(WorkflowRequest workflowRequest) {
+        ApplicationDTO applicationDTO = workflowRequest.getApplicationDTO();
         RepOrderDTO repOrderDTO = repOrderService.getRepOrder(workflowRequest);
         if (repOrderDTO == null) {
             log.error("Could not find rep order for request {}", workflowRequest);
-            throw new MaatOrchestrationException(workflowRequest.getApplicationDTO());
+            throw new MaatOrchestrationException(applicationDTO);
         }
         UserActionDTO userActionDTO = passportAssessmentMapper.getUserActionDTO(workflowRequest);
 
@@ -54,36 +65,40 @@ public class PassportAssessmentOrchestrationService {
         String assessmentId = passportAssessmentService.create(workflowRequest);
 
         try {
-            // TODO: Check what is being done about manage passport evidence, no ticket on backlog and evidence endpoint geared towards income???
-
+            workflowRequest.setApplicationDTO(maatCourtDataService.invokeStoredProcedure(
+                applicationDTO, workflowRequest.getUserDTO(),
+                StoredProcedure.MANAGE_PASSPORT_EVIDENCE));
             proceedingsService.determineMagsRepDecision(workflowRequest);
-
-            // TODO: Check crown repord called in stored procedure, should use CCP process rep order which looks very similar or CCP update application???
-
-            contributionService.calculate(workflowRequest);
-
+            workflowRequest.setApplicationDTO(contributionService.calculate(workflowRequest));
             proceedingsService.updateApplication(workflowRequest, repOrderDTO);
 
-            // TODO: Do we need to do this only if work reason isn't FMA as it is in the stored procedure???
-            workflowRequest.setApplicationDTO(maatCourtDataService.invokeStoredProcedure(
-                workflowRequest.getApplicationDTO(),
-                workflowRequest.getUserDTO(),
-                StoredProcedure.PROCESS_ACTIVITY_AND_GET_CORRESPONDENCE));
+            if (!NewWorkReason.FMA.equals(NewWorkReason.getFrom(applicationDTO.getPassportedDTO().getNewWorkReason().getCode()))) {
+                workflowRequest.setApplicationDTO(maatCourtDataService.invokeStoredProcedure(
+                    applicationDTO, workflowRequest.getUserDTO(),
+                    StoredProcedure.PROCESS_ACTIVITY_AND_GET_CORRESPONDENCE));
+            }
 
             AssessmentSummaryDTO assessmentSummaryDTO = assessmentSummaryService.getSummary(
-                workflowRequest.getApplicationDTO().getAssessmentDTO().getIojAppeal());
-            assessmentSummaryService.updateApplication(workflowRequest.getApplicationDTO(), assessmentSummaryDTO);
+                applicationDTO.getAssessmentDTO().getIojAppeal());
+            assessmentSummaryService.updateApplication(applicationDTO, assessmentSummaryDTO);
 
-            // TODO: Do we need to update date modified (app date completed in SP) and call ATS here similar to the IOJ flow???
+            // TODO: Need to update the ass date completed by calling patch rep order endpoint (use fin ass data from appDTO)
+            applicationService.updateDateModified(workflowRequest, applicationDTO);
+
+            ApplicationTrackingOutputResult applicationTrackingOutputResult = applicationTrackingMapper.build(
+                workflowRequest, repOrderDTO, AssessmentType.PASSPORT, RequestSource.PASSPORT_IOJ);
+            if (null != applicationTrackingOutputResult.getUsn()) {
+                applicationTrackingDataService.sendTrackingOutputResult(applicationTrackingOutputResult);
+            }
         } catch (Exception exception) {
             log.error("Create passport assessment post processing failed, rolling back...", exception);
             Sentry.captureException(exception);
 
             // TODO: Need to call rollback passport assessment and handle failure when ticket LCAM-1987 and follow on has been completed.
 
-            throw new MaatOrchestrationException(workflowRequest.getApplicationDTO());
+            throw new MaatOrchestrationException(applicationDTO);
         }
 
-        return workflowRequest.getApplicationDTO();
+        return applicationDTO;
     }
 }
